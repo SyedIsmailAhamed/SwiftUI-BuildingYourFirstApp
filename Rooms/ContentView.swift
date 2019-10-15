f771c2d19ce98e7cec81376f54a6aa91ebde376f
Syed Ismail Ahamed <syed.ismail@ia.gov.ae>
2019-10-15 09:40:49 +0400

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Syed Ismail Ahamed on 15/10/2019.
+//  Copyright © 2019 Syed Ismail Ahamed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
